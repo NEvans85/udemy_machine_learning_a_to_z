6660ae94fa6155961a79a3e2498963d32d71cd52
Nicholas Evans <nevans85.pro@gmail.com>
2017-12-05 07:57:27 -0800

    prepare templates

--- course_files/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.R
+++ course_files/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.R
@@ -2,22 +2,7 @@
 
 # Importing the Dataset
 dataset = read.csv('Data.csv')
-
-# Taking care of missing data
-dataset$Age = ifelse(is.na(dataset$Age),
-                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
-                     dataset$Age)
-dataset$Salary = ifelse(is.na(dataset$Salary),
-                        ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
-                        dataset$Salary)
-
-# Encode categorical data
-dataset$Country = factor(dataset$Country,
-                         levels = c('France', 'Spain', 'Germany'),
-                         labels = c(1, 2, 3))
-dataset$Purchased = factor(dataset$Purchased,
-                           levels = c('No', 'Yes'),
-                           labels = c(0, 1))
+# dataset = dataset[, 2:3]
 
 # Splitting the Dataset into the training and test sets
 # install.packages('caTools')
@@ -28,5 +13,5 @@ training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
 # Feature Scaling
-training_set[, 2:3] = scale(training_set[, 2:3])
-test_set[, 2:3] = scale(test_set[, 2:3])
+# training_set[, 2:3] = scale(training_set[, 2:3])
+# test_set[, 2:3] = scale(test_set[, 2:3])
